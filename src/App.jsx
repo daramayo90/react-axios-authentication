ecb712fe7bb6e6f09e12763911266365cc756355
daramayo90 <da.aramayo1990@gmail.com>
2022-04-17 22:14:32 -0500

    register form with validation created using react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Register from './components/register/Register';
+
+function App() {
+
+  return (
+    <main className="App">
+      <Register />
+    </main>
+  );
+}
+
+export default App;
